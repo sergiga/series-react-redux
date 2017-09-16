767506083aae0e91ee453e3da2485c80da09a585
Sergio Garcia <srgarcia91@gmail.com>
2017-09-16 19:26:55 +0200

    Split the entity reducer in actor and serie reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,16 +1,32 @@
-import merge from 'lodash/merge'
+import * as ActionTypes from '../actions';
+import merge from 'lodash/merge';
 import { combineReducers } from 'redux';
 
-const entities = (state = { series: {} }, action) => {
-  if (action.response && action.response.entities) {
-    return merge({}, state, action.response.entities)
+const mergeEntities = (state, action, entity) => {
+  if (action.response && action.response.entities && action.response.entities[entity]) {
+    return merge({}, state, action.response.entities[entity]);
   }
+  return state;
+}
 
-  return state
+const actors = (state = {}, action) => {
+  return mergeEntities(state, action, 'actors');
+}
+
+const series = (state = {}, action) => {
+  switch(action.type) {
+    case ActionTypes.SERIE_CAST_SUCCESS:
+      return state;
+    default:
+      return mergeEntities(state, action, 'series');
+  }
 }
 
 const reducer = combineReducers({
-  entities
+  entities: combineReducers({
+    actors,
+    series
+  })
 });
 
 export default reducer;
