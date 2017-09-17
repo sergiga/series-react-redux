ef112426b3cdefc1e99a8ae6b54150f5648e8d2c
Sergio Garcia <srgarcia91@gmail.com>
2017-09-17 19:01:51 +0200

    Add react router and Root component

---
+++ src/containers/Root.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Provider } from 'react-redux';
+import { Route, Switch } from 'react-router-dom';
+import App from '../views/App';
+
+const Root = ({ store }) => (
+  <Provider store={store}>
+    <App>
+      <Switch>
+      </Switch>
+    </App>
+  </Provider>
+);
+
+Root.propTypes = {
+  store: PropTypes.object.isRequired,
+};
+
+export default Root;
