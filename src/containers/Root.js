a22bb4feee6fb44e8db327059a771d54d07a9201
Sergio Garcia <srgarcia91@gmail.com>
2017-09-17 19:51:20 +0200

    Routing the SeriesPage to the root page

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -3,11 +3,14 @@ import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 import App from '../views/App';
+import SeriesPage from './SeriesPage';
 
 const Root = ({ store }) => (
   <Provider store={store}>
     <App>
       <Switch>
+        <Route name='series' path="/" component={ SeriesPage }/>
+        {/*<Route name='serie' path="/series/:id" component={ Serie }/>*/}
       </Switch>
     </App>
   </Provider>
