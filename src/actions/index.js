3a80e1bf091dcfe3fcde637232019586f2ab34de
Sergio Garcia <srgarcia91@gmail.com>
2017-09-21 17:34:05 +0200

    HOF to hold the action dispatcher and the dispatch in the same function

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,14 +4,16 @@ export const SEARCH_REQUEST = 'SEARCH_REQUEST';
 export const SEARCH_SUCCESS = 'SEARCH_SUCCESS';
 export const SEARCH_FAILURE = 'SEARCH_FAILURE';
 
-export const searchSerie = serie => ({
-  [API_MIDDLEWARE]: {
-    types: [ SEARCH_REQUEST, SEARCH_SUCCESS, SEARCH_FAILURE ],
-    endpoint: 'search/shows',
-    query: { q: serie },
-    schema: Schemas.SERIE_ARRAY
-  }
-});
+export const searchSerie = serie => dispatch => {
+  return dispatch(() => ({
+    [API_MIDDLEWARE]: {
+      types: [ SEARCH_REQUEST, SEARCH_SUCCESS, SEARCH_FAILURE ],
+      endpoint: 'search/shows',
+      query: { q: serie },
+      schema: Schemas.SERIE_ARRAY
+    }
+  }));
+}
 
 export const SERIE_PAGE_REQUEST = 'SERIE_PAGE_REQUEST';
 export const SERIE_PAGE_SUCCESS = 'SERIE_PAGE_SUCCESS';
