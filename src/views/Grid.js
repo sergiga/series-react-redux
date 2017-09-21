bc02ceecfe4a2f740c6faad3761b828a63ab8c96
Sergio Garcia <srgarcia91@gmail.com>
2017-09-21 14:03:40 +0200

    Added an on load more click event on Grid component

--- src/views/Grid.js
+++ src/views/Grid.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-function Grid (props) {
-  const { items, renderItem } = props;
-  
-  return(
-    <div className='grid-container'>
-      {items.map(item => <div key={item.id} className='grid-item'>{renderItem(item)}</div>)}    
-    </div>
-  );
+class Grid extends Component {
+  renderLoadMore(event) {
+    if(!event) { return null }
+
+    return(
+      <div className='grid--load-more'>
+        <button onClick={event}>LOAD MORE</button>
+      </div>
+    );
+  }
+
+  render() {
+    const { items, renderItem, onLoadMoreClick } = this.props;
+
+    return(
+      <div className='grid'>
+        <div className='grid-container'>
+          {items.map(item => <div key={item.id} className='grid-item'>{renderItem(item)}</div>)}    
+        </div>
+        {this.renderLoadMore(onLoadMoreClick)}
+      </div>
+    );
+  }
 }
 
 Grid.propTypes = {
