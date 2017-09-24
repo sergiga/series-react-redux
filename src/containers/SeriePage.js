29b4e29b21a8ffb5af4ac600fe485c4f300f412f
Sergio Garcia <srgarcia91@gmail.com>
2017-09-24 20:27:37 +0200

    Container component connected to the Redux store

---
+++ src/containers/SeriePage.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { loadSerie } from '../actions';
+import Serie from '../views/Serie';
+import Header from '../views/Header';
+
+class SeriePage extends Component {
+  static propTypes = {
+    serie: PropTypes.object
+  }
+  
+  componentWillMount() {
+    const serieID = this.props.match.params.id;
+
+    this.props.loadSerie(serieID, ['cast']);
+  }
+
+  render() {
+    const { serie } = this.props;
+
+    const serieComponent = serie ? <Serie {...serie} /> : null;
+
+    return (
+      <div>
+        {serieComponent}
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  const {
+    entities: { series, actors }
+  } = state;
+
+  const serieID = ownProps.match.params.id;
+  let serie = series[serieID];
+  
+  if(serie && serie.cast) {
+    serie.cast = serie.cast.map(castID => actors[castID]);
+  }
+
+  return {
+    serie
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  {
+    loadSerie
+  }
+)(SeriePage);
